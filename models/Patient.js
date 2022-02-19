ba44b0b6ed0b74c74e4353749314599ffaa91dfe
shanoysinc <shanoy96@gmail.com>
2022-02-19 08:41:52 -0800

    create patient model

---
+++ models/Patient.js
@@ -0,0 +1,38 @@
+// const mongoose = require("mongoose");
+// const { isEmail } = require("validator");
+// const bcrypt = require("bcrypt");
+import validator from "validator";
+import mongoose from "mongoose";
+const isEmail = validator.isEmail;
+
+const patientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+  },
+  occupation: {
+    type: String,
+  },
+  medicalHistoryDetails: {
+    type: String,
+  },
+  IP: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [(val) => isEmail(val), "Please enter a valid email"],
+  },
+  password: {
+    select: false,
+    type: String,
+    required: [true, "Please enter password"],
+    minlength: [6, "Minimum password length is 6 characters"],
+  },
+  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
+});
+
+export const Patient = mongoose.model("Patient", patientSchema);
