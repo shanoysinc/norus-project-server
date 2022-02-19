aaa5161a07ad66e7bd7f16e4778bc840d93ec6b0
shanoysinc <shanoy96@gmail.com>
2022-02-19 08:59:30 -0800

    import jsonwebtoken

--- helpers/token/token.js
+++ helpers/token/token.js
@@ -1,3 +1,4 @@
+import jsonwebToken from "jsonwebtoken";
 export const generateToken = (userId, userRole) => {
   const token = jsonwebToken.sign({ userId, userRole }, process.env.JWT_SECRET);
   return token;
