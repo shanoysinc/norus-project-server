36894b867f7abd6f012dbcfa411dacd01cca9be0
shanoysinc <shanoy96@gmail.com>
2022-02-20 00:49:22 -0800

    add token to doctor and patient model when login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -35,7 +35,10 @@ export const patientSignup = async (req, res) => {
     await patient.save();
     const token = generateToken(patient._id, Roles.PATIENT);
 
-    res.json({ patient, token });
+    await patient.set({ token });
+    await patient.save();
+
+    res.json({ patient });
   } catch (err) {
     res.json({
       error: true,
@@ -58,15 +61,36 @@ export const patientLogin = async (req, res) => {
     if (!passwordMatch) {
       throw Error("Please enter correct credentials");
     }
-
     const token = generateToken(patient._id, Roles.PATIENT);
 
-    res.json({ patient, token });
+    await patient.set({ token });
+    await patient.save();
+
+    res.json({ patient });
   } catch (err) {
     res.json({
       error: true,
       errorMessage: err.message,
     });
+  }
+};
+
+export const patientLogout = async (req, res) => {
+  try {
+    const { userId } = req.user;
+
+    if (!userId) {
+      throw Error("unathorize access");
+    }
+
+    const patient = await Patient.findOne({ _id: userId });
+
+    await patient.set({ token: "" });
+    await patient.save();
+
+    res.json({ success: true });
+  } catch (err) {
+    res.json({ success: false });
   }
 };
 
@@ -87,12 +111,33 @@ export const doctorLogin = async (req, res) => {
     }
 
     const token = generateToken(doctor._id, Roles.DOCTOR);
+    await doctor.set({ token });
+    await doctor.save();
 
-    res.json({ doctor, token });
+    res.json({ doctor });
   } catch (err) {
     res.json({
       error: true,
       errorMessage: err.message,
     });
+  }
+};
+
+export const doctorLogout = async (req, res) => {
+  try {
+    const { userId } = req.user;
+
+    if (!userId) {
+      throw Error("unathorize access");
+    }
+
+    const doctor = await Doctor.findOne({ _id: userId });
+
+    await doctor.set({ token: "" });
+    await doctor.save();
+
+    res.json({ success: true });
+  } catch (err) {
+    res.json({ success: false });
   }
 };
