6017037e71133c2a689dc4dcd4c75643e3db1c25
shanoysinc <shanoy96@gmail.com>
2022-02-20 00:51:03 -0800

    add patient route

--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { deletePatient } from "../controllers/patient.controller.js";
+import {
+  createAppointment,
+  deletePatient,
+  getAppointments,
+} from "../controllers/patient.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/");
 router.delete("/", verifyToken, deletePatient);
+
+router.get("/appointment", verifyToken, getAppointments);
+router.post("/appointment", verifyToken, createAppointment);
 
 export default router;
