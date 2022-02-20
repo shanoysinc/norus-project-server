57dd4092221df624b016b010cc0cc6320233a645
shanoysinc <shanoy96@gmail.com>
2022-02-19 23:35:30 -0800

    add logout route for patients

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,11 +3,14 @@ import {
   doctorLogin,
   patientLogin,
   patientSignup,
+  patientLogout,
 } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
 router.post("/patient/signup", patientSignup);
 router.post("/patient/login", patientLogin);
+router.post("/patient/logout", verifyToken, patientLogout);
 
 router.post("/doctor/login", doctorLogin);
 
