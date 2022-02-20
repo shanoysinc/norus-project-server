7b4fff134f60b974d70c158fa7e5a7248d495fcd
shanoysinc <shanoy96@gmail.com>
2022-02-20 00:50:27 -0800

    update appointment model

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
 const appointmentSchema = new mongoose.Schema({
-  complain: {
+  symptom: {
     type: String,
     required: [true, "Please enter a reason's for visiting the doctor"],
   },
   details: {
     type: String,
   },
-  patientIP: {
-    type: String,
+  date: {
+    type: Date,
+  },
+  approve: {
+    type: Boolean,
+    default: false,
   },
   doctor: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
   patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
