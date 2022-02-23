9ceee602d4f7ba59c99c1ee8968313241d2d582c
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:19:44 -0800

    use authorization bearer instead of x-access-token

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -5,11 +5,12 @@ import { Doctor } from "../models/Doctor.js";
 import { Patient } from "../models/Patient.js";
 
 export const verifyToken = async (req, res, next) => {
-  const token = req.headers["x-access-token"];
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
-  }
   try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+      return res.status(403).send("A token is required for authentication");
+    }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const { userRole, userId } = decoded;
@@ -34,6 +35,7 @@ export const verifyToken = async (req, res, next) => {
     }
     return res.status(403).send("unauthorize access");
   } catch (err) {
-    res.status(401).send({ error: true, errorMessage: err.message });
+    console.log(err);
+    return res.status(401).send({ error: true, errorMessage: err.message });
   }
 };
