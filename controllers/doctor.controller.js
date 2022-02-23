1b28c551201e76295e03365472fca0d11081d85a
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:19:08 -0800

    add get doctor route

--- controllers/doctor.controller.js
+++ controllers/doctor.controller.js
@@ -1,6 +1,21 @@
 import { Appointment } from "../models/Appointment.js";
+import { Doctor } from "../models/Doctor.js";
 import { Patient } from "../models/Patient.js";
 
+export const getDoctor = async (req, res) => {
+  try {
+    const { userId } = req.user;
+
+    const doctor = await Doctor.findOne({ _id: userId });
+
+    res.json({ auth: true, doctor });
+  } catch (err) {
+    res.json({
+      error: true,
+      errorMessage: err.message,
+    });
+  }
+};
 export const getDoctorPatients = async (req, res) => {
   try {
     const { userId } = req.user;
@@ -15,13 +30,36 @@ export const getDoctorPatients = async (req, res) => {
     });
   }
 };
+
 export const getDoctorAppointments = async (req, res) => {
   try {
     const { userId } = req.user;
 
-    const appointments = await Appointment.find({ doctor: userId });
+    const appointments = await Appointment.find({ doctor: userId }).populate(
+      "patient"
+    );
 
     res.json({ appointments });
+  } catch (err) {
+    res.json({
+      error: true,
+      errorMessage: err.message,
+    });
+  }
+};
+
+export const updateDoctorAppointments = async (req, res) => {
+  try {
+    const { currentAppointmentId, approve } = req.body;
+
+    const appointment = await Appointment.findOneAndUpdate(
+      { _id: currentAppointmentId },
+      {
+        $set: { approve: !approve },
+      }
+    );
+
+    res.json({ appointment });
   } catch (err) {
     res.json({
       error: true,
