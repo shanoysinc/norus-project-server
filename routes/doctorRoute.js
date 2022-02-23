91774a89c9cbfb6284ee0f43b331453388e343e8
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:20:34 -0800

    add route for updating doctors appointment

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -3,17 +3,27 @@ import { Roles } from "../const/index.js";
 import {
   getDoctorAppointments,
   getDoctorPatients,
+  getDoctor,
+  updateDoctorAppointments,
 } from "../controllers/doctor.controller.js";
 import { authRole } from "../middleware/authRole.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 const doctorRole = Roles.DOCTOR;
+router.get("/", verifyToken, authRole(doctorRole), getDoctor);
 router.get(
   "/appointments",
   verifyToken,
   authRole(doctorRole),
   getDoctorAppointments
+);
+
+router.patch(
+  "/appointments",
+  verifyToken,
+  authRole(doctorRole),
+  updateDoctorAppointments
 );
 router.get("/patients", verifyToken, authRole(doctorRole), getDoctorPatients);
 
