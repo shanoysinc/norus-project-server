1782e7cebc7fd39eb0cd884cb2387db06d499fb0
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:20:10 -0800

    remove roles

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,31 +1,19 @@
 import express from "express";
-import { Roles } from "../const/index.js";
 import {
   doctorLogin,
   patientLogin,
   patientSignup,
-  patientLogout,
-  doctorLogout,
+  userLogout,
 } from "../controllers/auth.controller.js";
-import { authRole } from "../middleware/authRole.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-const doctorRole = Roles.DOCTOR;
-const patientRole = Roles.PATIENT;
-
 router.post("/patient/signup", patientSignup);
 router.post("/patient/login", patientLogin);
 
-router.post(
-  "/patient/logout",
-  verifyToken,
-  authRole(patientRole),
-  patientLogout
-);
+router.post("/user/logout", verifyToken, userLogout);
 
 router.post("/doctor/login", doctorLogin);
-router.post("/doctor/logout", authRole(doctorRole), doctorLogout);
 
 export default router;
