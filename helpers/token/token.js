0e209558004f059bf65dec726562742fbeefdba6
shanoysinc <shanoy96@gmail.com>
2022-02-24 10:57:26 -0800

    add expire date on token

--- helpers/token/token.js
+++ helpers/token/token.js
@@ -1,5 +1,11 @@
 import jsonwebToken from "jsonwebtoken";
 export const generateToken = (userId, userRole) => {
-  const token = jsonwebToken.sign({ userId, userRole }, process.env.JWT_SECRET);
+  const token = jsonwebToken.sign(
+    { userId, userRole },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
   return token;
 };
