035359c1603b91f534183122a4931e146c33c6bb
shanoysinc <shanoy96@gmail.com>
2022-02-23 19:34:13 -0800

    add timeline model

---
+++ models/PatientTimline.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const patientTimlineSchema = new mongoose.Schema({
+  approve: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  appointment: { type: mongoose.Schema.Types.ObjectId, ref: "Appointment" },
+  patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
+});
+
+export const PatientTimlineSchema = mongoose.model(
+  "PatientTimeline",
+  patientTimlineSchema
+);
