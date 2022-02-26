f16d21595e25f35a36823d0d1c30bfe60ffe888c
shanoysinc <shanoy96@gmail.com>
2022-02-26 01:33:05 -0800

    return response to prevent next call

--- middleware/authRole.js
+++ middleware/authRole.js
@@ -1,7 +1,7 @@
 export function authRole(role) {
   return (req, res, next) => {
     if (req.user.userRole !== role) {
-      res.status(401).send("Not allowed");
+      return res.status(401).send("Not allowed");
     }
 
     next();
